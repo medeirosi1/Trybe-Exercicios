f984dfd01154a4a751723523469693edb2cacbba
Thiago <medeiroshs@hotmail.com>
2021-11-30 18:34:05 -0300

    Dia 03 - JavaScript - Lógica de Programação e Algoritmos - Exercicio

---
+++ Fundamentos/Javascript/Dia 03 - Prática.js
@@ -0,0 +1,63 @@
+/* let n = 5;
+for(i = 1; i <= n; i += 1){
+   console.log("*****");       
+} */
+ 
+/* let n = 5;
+let asterisco = "*";
+for(i = 1; i <= n; i += 1){
+    if(i <= 1){
+        console.log(asterisco);
+    }
+    if(i == 2){
+        console.log(asterisco+asterisco);
+    }
+    if(i == 3){
+        console.log(asterisco+asterisco+asterisco);
+    }   
+    if(i == 4){
+        console.log(asterisco+asterisco+asterisco+asterisco);
+    }    
+    if(i == 5){
+        console.log(asterisco+asterisco+asterisco+asterisco+asterisco);
+    }     
+} */
+
+/* let n = 5;
+let asterisco = "*";
+for(i = 1; i <= n; i += 1){
+    if(i <= 1){
+        console.log("    " + asterisco);
+    }
+    if(i == 2){
+        console.log("   " + asterisco+asterisco);
+    }
+    if(i == 3){
+        console.log("  " + asterisco+asterisco+asterisco);
+    }   
+    if(i == 4){
+        console.log(" " + asterisco+asterisco+asterisco+asterisco);
+    }    
+    if(i == 5){
+        console.log(asterisco+asterisco+asterisco+asterisco+asterisco);
+    }     
+} */
+
+/* let n = 5;
+let asterisco = "*";
+let linha = "";
+for(i = 0; i < n; i += 1){
+   linha = linha + asterisco;    
+};
+
+for(i = 0; i < n; i += 1){
+    console.log(linha);
+} */
+
+let n = 5;
+let asterisco = "*";
+let linha = "";
+for(i = 0; i < n; i += 1){
+   linha = linha + asterisco;  
+   console.log(linha);    
+};
