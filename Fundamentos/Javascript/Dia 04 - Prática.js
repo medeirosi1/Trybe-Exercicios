61607f38553ec9e79ed3adc5c6dc165a31179146
Thiago <medeiroshs@hotmail.com>
2021-12-01 19:10:50 -0300

    Dia 04 - JavaScript - Objetos e funções - Exercicios

---
+++ Fundamentos/Javascript/Dia 04 - Prática.js
@@ -0,0 +1,22 @@
+// Parte I - Objetos e For/In
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
+  console.log("Bem vinda " + info.personagem);
+
+  // 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+  info.recorrente = "Sim";
+  console.log(info); 
+  
+  // 3 - Faça um for/in que mostre todas as chaves do objeto.
+
+  for (let key in info) {
+      console.log()
+  }
