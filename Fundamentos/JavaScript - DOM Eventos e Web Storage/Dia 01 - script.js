c2b0c0f780551e02ac53d2b88aa4db183d85c6e6
Thiago <medeiroshs@hotmail.com>
2021-12-06 11:19:07 -0300

    Dia 01 - JavaScript - DOM e seletores

---
+++ Fundamentos/JavaScript - DOM Eventos e Web Storage/Dia 01 - script.js
@@ -0,0 +1,36 @@
+function corFundoTitle (color){
+    let corFundo = document.querySelector("#header-container");
+corFundo.style.backgroundColor = color;
+}
+corFundoTitle("MediumSeaGreen");
+
+function corFundoEmergency (color){
+    let fundoCor = document.querySelector(".emergency-tasks");
+    fundoCor.style.backgroundColor = color;
+}
+corFundoEmergency("Salmon");
+
+function corFundoNoEmergency (color){
+    let colorFundo = document.querySelector(".no-emergency-tasks");
+    colorFundo.style.backgroundColor = color;
+}
+corFundoNoEmergency("Khaki")
+
+function footerFundo (color){
+    let footer = document.querySelector("#footer-container");
+    footer.style.backgroundColor = color;
+}
+footerFundo("green");
+
+function corTextFundoEmergency (color1, color2) {
+    let h3 = document.querySelectorAll("h3");
+    for (let i = 0; i <= h3.length; i += 1){
+        if (i < 2){
+            h3[i].style.backgroundColor = color1;
+        } else {
+            h3[i].style.backgroundColor = color2;
+        }
+    }
+} 
+corTextFundoEmergency ("purple", "black");
+
